c77384498a87b6c94ad23b45a225d1cbd6f3dc6f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-27 19:35:58 -0500

    Display the same sprite but using ImageEnv

---
+++ src/image_env.py
@@ -0,0 +1,72 @@
+from enum import Enum
+from dataclasses import dataclass
+import pygame as pg
+import os
+
+
+@dataclass(frozen=True)
+class ImageInfo:
+    image: pg.Surface
+    rect: pg.Rect
+
+
+class Tile(Enum):
+    STAIRS_DOWN = pg.math.Vector2(5, 2)
+
+
+class ImageEnv:
+    """A class bundling all image and tile data into a consistent
+    whole.
+
+    Fields:
+
+    scale: The overall scale factor applied to the game display as a
+    whole.
+
+    size: The *original* size of each sprite in the given tileset.
+
+    window: The window used to display the game.
+
+    """
+    def __init__(self,
+                 win_width: float,
+                 win_height: float,
+                 scale: float,
+                 size: float):
+        self.scale = scale
+        self.size = size * scale
+        self.window = pg.display.set_mode((win_width * scale,
+                                           win_height * scale))
+
+    def load_tile(self, tile: Tile, tileset_info: ImageInfo) -> ImageInfo:
+        """Load a single, discrete tile from a tileset.
+
+        """
+
+        x_tile, y_tile = Tile.STAIRS_DOWN.value
+
+        rect = pg.Rect(x_tile * self.size,
+                       y_tile * self.size,
+                       self.size,
+                       self.size)
+
+        image = tileset_info.image.subsurface(rect)
+
+        return ImageInfo(image, rect)
+
+    def load_image(self, data_dir, name) -> ImageInfo:
+        """Helper function for loading images."""
+
+        fullname = os.path.join(data_dir, name)
+        image = pg.image.load(fullname)
+
+        size_x, size_y = image.get_size()
+        scale_factor = (size_x * self.scale, size_y * self.scale)
+        image = pg.transform.scale(image, scale_factor)
+
+        try:
+            image = image.convert()
+        except Exception as e:
+            print(f"\n---> {e} :)")
+
+        return ImageInfo(image, image.get_rect())
--- src/main.py
+++ src/main.py
@@ -1,62 +1,22 @@
 import pygame as pg
 import os
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class ImageInfo:
-    image: pg.Surface
-    rect: pg.Rect
-
-
-tile_legend = {
-    "stairs_down": pg.math.Vector2(5, 2)
-}
+import image_env
 
 
 # Use platform independent file paths.
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "../data")
 
+ienv = image_env.ImageEnv(win_width=128,
+                          win_height=128,
+                          scale=2, size=16)
 
-def load_image(name, scale=1) -> ImageInfo:
-    """Helper function for loading images."""
+window = ienv.window
+tileset_info = ienv.load_image(data_dir, "ff_castle.png")
+tile_info = ienv.load_tile(image_env.Tile.STAIRS_DOWN,
+                           tileset_info)
 
-    fullname = os.path.join(data_dir, name)
-    image = pg.image.load(fullname)
-
-    size_x, size_y = image.get_size()
-    size = (size_x * scale, size_y * scale)
-    image = pg.transform.scale(image, size)
-
-    try:
-        image = image.convert()
-    except Exception as e:
-        print(f"\n---> {e} :)")
-
-    return ImageInfo(image, image.get_rect())
-
-
-scale = 2
-window = pg.display.set_mode((128 * scale, 128 * scale))
-tileset_info = load_image("ff_castle.png", scale)
-size = 16 * scale
-
-def load_tile(tile_name: str,
-              tileset_info: ImageInfo,
-              size: float) -> ImageInfo:
-
-    x_tile, y_tile = tile_legend[tile_name]
-    rect = pg.Rect(x_tile * size, y_tile * size, size, size)
-
-    image = tileset_info.image.subsurface(rect)
-
-    return ImageInfo(image, rect)
-
-
-tile_info = load_tile("stairs_down", tileset_info, size)
-
-window.blit(tile_info.image, (size, size))
+window.blit(tile_info.image, (ienv.size, ienv.size))
 
 
 def mainloop():

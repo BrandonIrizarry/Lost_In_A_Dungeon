3416adaf1d92d26707650eed9c5809bb0c78ffb7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-01-09 17:14:37 -0500

    Move Spritesheet class to its own file

--- src/main.py
+++ src/main.py
@@ -3,52 +3,9 @@ import random
 import constants as cs
 from tiledef import TileDef
 from typedefs import Point
+from spritesheet import Spritesheet
 import maze
 from pygame.math import Vector2
-
-
-class Spritesheet:
-    """Bundle a sprite sheet image together with a method for fetching
-    sprites.
-
-    Fields:
-
-    sheet: The image corresponding to the sprite sheet. This is loaded
-    once upon initialization.
-
-    """
-    def __init__(self, filename):
-        self.sheet = pygame.image.load(filename).convert()
-
-    def get(self, tile_def: TileDef) -> pygame.Surface:
-        """Fetch a single, discrete, tile from 'self.sheet'.
-
-        Return the tile as a pygame.Surface object.
-
-        It's assumed that each tile is a square of 'size'x'size'
-        pixels inside the spritesheet.
-
-        """
-        tile_data = tile_def.value
-        x_tile, y_tile = tile_data[0]
-        color_key = tile_data[1]
-
-        rect = pygame.Rect(x_tile * cs.TILE_LEN,
-                           y_tile * cs.TILE_LEN,
-                           cs.TILE_LEN,
-                           cs.TILE_LEN)
-
-        image = self.sheet.subsurface(rect)
-
-        if color_key:
-            image.set_colorkey(color_key)
-
-        return pygame.transform.scale_by(image, cs.SCALE_FACTOR)
-
-    def get_all(self, tile_defs: list[TileDef]) -> list[pygame.Surface]:
-        """Convert the given list of tile defs to a list of surfaces."""
-
-        return [self.get(tile_def) for tile_def in tile_defs]
 
 
 class MovingThing(pygame.sprite.Sprite):

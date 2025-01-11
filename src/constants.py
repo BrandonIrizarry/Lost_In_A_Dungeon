18618d6b8d5182ae980ffab084afc5ba1b957159
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-01-10 19:59:13 -0500

    Add new GRID_FACTOR constant + use within constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,14 +4,15 @@ GRID_X = 10
 GRID_Y = 10
 NUM_TILES_X = 3 * GRID_X + 1
 NUM_TILES_Y = 3 * GRID_Y + 1
+GRID_FACTOR = TILE_LEN * SCALE_FACTOR
 
 
 def compute_pixel_coords(x_grid: int, y_grid: int) -> tuple[int, int]:
     """Return inputs scaled to pixel units, appropriate for display on
     the pygame screen."""
 
-    xs = x_grid * TILE_LEN * SCALE_FACTOR
-    ys = y_grid * TILE_LEN * SCALE_FACTOR
+    xs = x_grid * GRID_FACTOR
+    ys = y_grid * GRID_FACTOR
 
     return xs, ys
 
@@ -20,7 +21,7 @@ def compute_grid_coords(x_pixel: int, y_pixel: int) -> tuple[int, int]:
     """Return inputs scaled to grid units, appropriate for handling
     sprite logic."""
 
-    xs = x_pixel // (TILE_LEN * SCALE_FACTOR)
-    ys = y_pixel // (TILE_LEN * SCALE_FACTOR)
+    xs = x_pixel // GRID_FACTOR
+    ys = y_pixel // GRID_FACTOR
 
     return xs, ys
